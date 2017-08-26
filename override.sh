c00303df5c56d75930d2224467fd17b73c0c6a52
yerejm <github@thelostyerejm.com>
2017-08-26 13:45:26 +1000

    customise boxcutter builds with overrides

---
+++ override.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+CP=/usr/local/bin/gcp
+filelist=$(cd override && find . -type f)
+for f in ${filelist}; do
+    if [ -f "${f}" ]; then
+        # override only if checksum expected
+        target_sha=$(shasum -a 512 "${f}" | awk '{print $1}')
+        override_sha=$(grep "${f}" ${0} | awk '{print $2}')
+        if [ "${target_sha}" = "${override_sha}" -o -n "${FORCE}" ]; then
+            echo "Overriding existing ${f}"
+            (cd override && ${CP} --parents "${f}" ..)
+        else
+            echo "WARNING!!! Checksum mismatch for ${f}"
+        fi
+    else
+        # file only exists in override
+        echo "Applying new ${f}"
+        (cd override && ${CP} --parents "${f}" ..)
+    fi
+done
+
+# override file list with sha512s of files expected to be replaced
+# update with shasum -a 512 <file>
+#SHA512:./debian/debian9.json 90df71f79da9df902143a678b48d48e53a613fbc39ac3bd08983759f38a4b3627af267d8886bce120a72c5f5c8f43e9e56333414d297292ef50d165982dcf08c
+#SHA512:./debian/script/desktop.sh f18f96bb136e48fcd16437f36f75ef76a99b5e8d3386eebc2e947799c7a0a882f286d97ace59e83506b673e772b96bf02aff51c09a135f0e0946bdbb53529f66
+#SHA512:./windows/floppy/eval-win10x64-enterprise/Autounattend.xml e64ef59f27dd74c670a7e4ce901974cbceba8c61eb2187bb2757e4fda6542543841badbc09f4145143add850e2648a022bd5c2fed816d063f4bfa6999570187c
+#SHA512:./windows/script/vmtool.bat 6cd130c9ae50e5afb562ae96cf9e58037fa9de79ae4f2dd1df697577d190ae49dd83befe7ce333535018274fc8239c5a28d5ec7f1880ca899e59381272cc865f
---
+++ override/debian/script/desktop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ ! "$DESKTOP" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
+  exit
+fi
+
+apt-get install -y task-lxde-desktop
+
+# just in case the previous command failed to download all packages do it again
+apt-get install -y task-lxde-desktop
+
+LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
+
+# Configure lightdm autologin.
+
+if [ -f $LIGHTDM_CONFIG ]; then
+    sed -i s/^#autologin-user=$/autologin-user=vagrant/ $LIGHTDM_CONFIG
+    sed -i s/^#autologin-user-timeout=0$/autologin-user-timeout=0/ $LIGHTDM_CONFIG
+fi
+
+# Need to disable NetworkManager because it overwrites vagrant's
+# settings in /etc/resolv.conf with empty content in the first boot.
+# So, DNS doesn't work on the first boot.
+#
+# Maybe there is a better solution then disabling the service.
+systemctl disable NetworkManager.service
+
+echo "==> Removing desktop components"
+apt-get -y purge gnome-getting-started-docs clipit xscreensaver
+apt-get -y purge $(dpkg --get-selections | grep -v deinstall | grep libreoffice | cut -f 1)
+apt -y autoremove
