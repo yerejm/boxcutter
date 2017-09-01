178fad9a1d5ab6488f0cd93f77b356da73834926
yerejm <github@thelostyerejm.com>
2017-09-01 17:48:03 +1000

    debian: use i3wm

--- override/debian/script/desktop.sh
+++ override/debian/script/desktop.sh
@@ -4,10 +4,10 @@ if [[ ! "$DESKTOP" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
   exit
 fi
 
-apt-get install -y task-lxde-desktop
+apt-get install -y task-lxde-desktop i3
 
 # just in case the previous command failed to download all packages do it again
-apt-get install -y task-lxde-desktop
+apt-get install -y task-lxde-desktop i3
 
 LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
 
@@ -16,6 +16,7 @@ LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
 if [ -f $LIGHTDM_CONFIG ]; then
     sed -i s/^#autologin-user=$/autologin-user=vagrant/ $LIGHTDM_CONFIG
     sed -i s/^#autologin-user-timeout=0$/autologin-user-timeout=0/ $LIGHTDM_CONFIG
+    sed -i s/^#user-session=default$/user-session=i3/ $LIGHTDM_CONFIG
 fi
 
 # Need to disable NetworkManager because it overwrites vagrant's
